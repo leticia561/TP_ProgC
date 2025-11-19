543428f49f805133c851f36231243d962373ebe1
leticia561 <leticiahadiouche1@gmail.com>
2025-11-19 15:05:27 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,33 @@
+cat > pyramide.c << 'EOF'
+#include <stdio.h>
+
+int main() {
+    int n = 5; // hauteur de la pyramide, tu peux changer cette valeur
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        // afficher les espaces pour centrer les nombres
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // afficher les nombres croissants
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // afficher les nombres décroissants
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Génération de la pyramide terminée.\n");
+    return 0;
+}
+EOF
+gcc -o pyramide pyramide.c
+./pyramide
 
